838bf122f0eacc445e9b47d313e7967c3146141a
jbmc99 <127203600+jbmc99@users.noreply.github.com>
2024-07-13 14:09:27 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "custom-red": "#FF5A60",
+      },
+    },
+  },
+  plugins: [],
+};
